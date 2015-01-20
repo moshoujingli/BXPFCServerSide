2c122ddf47548e9d355c75d440e63f2b099d86e0
BiXiaopeng <bixiaopeng2007@gmail.com>
2015-01-20 22:57:14 +0900

    add[]:remove and check

--- handler.js
+++ handler.js
@@ -1,6 +1,29 @@
 var connection = require('./connection')
 function exist (request,response) {
-    
+    connection(function(databaseConnection) {
+    databaseConnection.collection('contracts', function(error, collection) {
+            if (error){
+                console.warn(error.message);
+                response.writeHead(400, {"Content-Type": "text/html"});
+                response.end('server error(1):'+error.message);
+            }
+            var uuids = request.body;
+            console.warn(uuids);
+            collection.find({_id:{$in:uuids}},
+                function (err, cursor) {
+                if (err){
+                    console.warn(err.message);
+                    response.writeHead(400, {"Content-Type": "text/html"});
+                    response.end('server error(2):'+err.message);
+                }else{
+                    cursor.toArray(function  (err,items) {
+                        response.writeHead(200, {"Content-Type": "application/json"});
+                        response.end(JSON.stringify(items));
+                    });
+                }
+            });
+        });
+    });    
 }
 function add (request,response) {
     connection(function(databaseConnection) {
@@ -8,15 +31,15 @@ function add (request,response) {
             if (error){
                 console.warn(error.message);
                 response.writeHead(400, {"Content-Type": "text/html"});
-                response.end('server error(1)');
+                response.end('server error(1):'+error.message);
             }
             var users = request.body;
             console.warn(users);
             collection.insert(users,{w:1}, function (err, doc) {
                 if (err){
-                    console.warn(error.message);
+                    console.warn(err.message);
                     response.writeHead(400, {"Content-Type": "text/html"});
-                    response.end('server error(2)');
+                    response.end('server error(2):'+err.message);
                 }else{
                     response.writeHead(200, {"Content-Type": "application/json"});
                     response.end(JSON.stringify(doc));
@@ -25,7 +48,30 @@ function add (request,response) {
         });
     });
 }
-function remove (request,response) {}
+function remove (request,response) {
+    connection(function(databaseConnection) {
+    databaseConnection.collection('contracts', function(error, collection) {
+            if (error){
+                console.warn(error.message);
+                response.writeHead(400, {"Content-Type": "text/html"});
+                response.end('server error(1):'+error.message);
+            }
+            var uuids = request.body;
+            console.warn(uuids);
+            collection.remove({_id:{$in:uuids}},
+                function (err, result) {
+                if (err){
+                    console.warn(error.message);
+                    response.writeHead(400, {"Content-Type": "text/html"});
+                    response.end('server error(2):'+error.message);
+                }else{
+                    response.writeHead(200, {"Content-Type": "application/json"});
+                    response.end(JSON.stringify(result));
+                }
+            });
+        });
+    }); 
+}
 
 
 exports.exist = exist
